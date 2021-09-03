60fb4f34ee5acc7b1d11316321c0a2679a69a34b
Sagar Pol <76035885+sagarpol3008@users.noreply.github.com>
2021-09-03 13:36:12 +0530

    Add files via upload

---
+++ Day 1/printInteger.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter integer: " << endl;
+    cin >> n;
+    cout << "Given integer that you have entered is:  " << n;
+    return 0;
+}
